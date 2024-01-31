5e3f78707aa860461c033435a1f7eaf4fbcf4bb0
Kai Götte <kai.goe@outlook.de>
2024-01-31 20:59:31 +0100

    added components editpage and homepage

---
+++ src/app/editpage/editpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './editpage.component.html',
+  styleUrl: './editpage.component.css'
+})
+export class EditpageComponent {
+
+}
